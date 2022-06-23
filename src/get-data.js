d3452b69bb5a2d5db3fc9f57d5258a69116952df
Gustavo Lira <gu.lirar23@gmail.com>
2022-06-23 16:08:55 -0500

    refactor: add an dispatch eventa and customEvent to send data to show-element

--- src/get-data.js
+++ src/get-data.js
@@ -1,5 +1,4 @@
 import { LitElement, html } from "lit-element";
-import "./show-element.js"
 
 class GetData extends LitElement{
     static get properties(){
@@ -10,37 +9,21 @@ class GetData extends LitElement{
     }
     constructor(){
         super();
-        this.url = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
     }
-    connectedCallback(){
-        super.connectedCallback();
-        if(!this.data){
-            this.getData();
-        }
+    firstUpdated(){ 
+        this.getData();
     }
-    render(){
-        return html`
-            ${this.data.map(item => {
-                console.log(item);
-                html`<show-elements 
-                        name="${item.name}"
-                        type="${item.type}"
-                        desc:"${item.desc}"
-                        race: "${item.race}"
-                        archetype: "${item.archetype}"
-                        image_url: "${item.card_images.image_url}"
-                        amazon_price: "${item.card_prices.amazon_price}"
-                        coolstuffinc_price: "${item.card_prices.coolstuffinc_price}"
-                    >
-                    </show-elements>`
-            })}
-        `
+    _sendData(data){
+        this.dispatchEvent(new CustomEvent('ApiCall',{
+            detail: {data}, bubbles: true, composed: true
+        }))
     }
     async getData(){
         const response = await fetch(this.url);
         const jsonResponse = await response.json();
         this.data = jsonResponse.data;
         console.log(this.data);
+        this._sendData(this.data);
     }
 }
 
