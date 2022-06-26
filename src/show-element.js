cdaa784698191ad0ceb79eaee58edd294ec6b367
Gustavo Lira <gu.lirar23@gmail.com>
2022-06-26 16:51:59 -0500

    refacto: now the data is obtenined from filter-nav component

--- src/show-element.js
+++ src/show-element.js
@@ -1,45 +1,87 @@
 import { LitElement, html, css } from "lit-element";
-import "./get-data.js"
+import "./get-data.js";
+import "./filter-nav.js";
 
 class ShowElement extends LitElement{
     static get properties(){
         return{
-            name: { type: String},
-            type: {type: String},
-            desc: { type: String},
-            race: { type: String},
-            archetype: { type: String},
-            image_url: { type: String},
-            amazon_price: { type: String},
-            coolstuffinc_price: { type: String},
-            data: {type: Array}
+            data: {type: Array},
         }
     }
-    constructor(){
+    static get styles(){
+        return css`
+        .cards-container{
+            display: grid;
+            gap: 32px;
+            grid-template-columns: repeat(auto-fill, minmax(432px, 1fr));
+            justify-content: space-around;
+            justify-items: center;
+        }
+        .card-info{
+            width: 300px;
+            height: 292px;
+            padding: 16px;
+            background-color: white;
+            overflow-y: scroll;
+        }
+        .card-wrapper{
+            position: relative;
+            display: flex;
+        }
+        .card-wrapper-image{
+            padding: 16px;
+            height: 292px;
+            object-fit: cover;
+        }
+        .card-image{
+            width: 200px;
+            object-fit: cover;
+        }
+        .sub-info{
+            display: flex;
+            flex-wrap: wrap;
+            gap: 8px;
+        }
+        `
+    }
+    constructor(){    
         super();
         this.data = [];
-        this.addEventListener('ApiCall', e => {
+        this.addEventListener('FilterApiCall', e => {
             this.data = e.detail.data;
         })
     }
     render(){
         return html`
             <get-data url="https://db.ygoprodeck.com/api/v7/cardinfo.php"></get-data>
-            <h1>Show elements from an api</h1>
-            ${this.data.filter((item, index) => index < 30).map(item => html`
-            <div>
-                <h3>${item.name}</h3>
-                <p>${item.type}</p>
-                <p>${item.desc}</p>
-                <p>${item.race}</p>
-                <p>${item.archetype}</p>
-                <img src="${item.card_images[0].image_url}">
-                <div>
-                    <p>${item.amazon_price}</p>
-                    <p>${item.coolstuffinc_price}</p>
+            <h1>Yugioh cards list</h1>
+            <filter-nav></filter-nav>
+            <div class="cards-container">
+                ${this.data.map((item, index) => 
+                html`
+                <div class="card-wrapper">
+                    <div class="card-wrapper-image">
+                        <img class="card-image" src="${item.card_images[0].image_url}" />
+                    </div>
+                    <div class="card-info">
+                        <h3><strong>Name:</strong> ${item.name}</h3>
+                        <p><strong>Description:</strong> ${item.desc}</p>
+                        <div class="sub-info">
+                            <p><strong>Type:</strong> ${item.type}</p>
+                            <p><strong>Race:</strong> ${item.race}</p>
+                            <p><strong>Archetype:</strong> ${item.archetype ? item.archetype : "N/A"}</p>
+                            <p><strong>Attribute:</strong> ${item.attribute}</p>
+                        </div>
+                        <div>
+                            <h4>Prices</h4>
+                            <div>
+                                <p><strong>Amazon:</strong> ${item.card_prices[0].amazon_price}</p>
+                                <p><strong>Cool Stuff Inc:</strong> ${item.card_prices[0].coolstuffinc_price}</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            
             `)}
         `
     }
